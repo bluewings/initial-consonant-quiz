803fdc978d92874882e64138949eb580eea173b3
sungwon.cha <mygenie3@naver.com>
2020-03-20 04:27:18 +0900

    Updates

---
+++ precache-manifest.7b1b7b0e6ff38732dcf464d41b7aa70e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1d2e35c2c1ca45022b8e7138c46e39",
+    "url": "/initial-consonant-quiz/index.html"
+  },
+  {
+    "revision": "40fbf713891600fba7b0",
+    "url": "/initial-consonant-quiz/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "facb260b7f65ae366a39",
+    "url": "/initial-consonant-quiz/static/css/main.ad588db8.chunk.css"
+  },
+  {
+    "revision": "40fbf713891600fba7b0",
+    "url": "/initial-consonant-quiz/static/js/2.2f727daa.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/initial-consonant-quiz/static/js/2.2f727daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "facb260b7f65ae366a39",
+    "url": "/initial-consonant-quiz/static/js/main.06efa74c.chunk.js"
+  },
+  {
+    "revision": "196c6d69d326c361d37d",
+    "url": "/initial-consonant-quiz/static/js/runtime-main.4a64c11c.js"
+  }
+]);
