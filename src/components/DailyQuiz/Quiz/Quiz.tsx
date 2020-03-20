fa04b101b0ccf07c6fab0df951c6bea07991d73e
sungwon.cha <mygenie3@naver.com>
2020-03-20 10:21:44 +0900

    fix

--- src/components/DailyQuiz/Quiz/Quiz.tsx
+++ src/components/DailyQuiz/Quiz/Quiz.tsx
@@ -26,27 +26,28 @@ function Quiz(props: any) {
   return (
     <div className={styles.root}>
       <div className={styles.no}>{no}</div>
-      <div className={styles.cards}>
-        {chars.map((f: any, i: number) => (
-          <Card key={i} {...f} open={showAns || f.timing <= level} />
-        ))}
+      <div className={styles.wrap}>
+        <div className={styles.cards}>
+          {chars.map((f: any, i: number) => (
+            <Card key={i} {...f} open={showAns || f.timing <= level} />
+          ))}
+        </div>
+        {showHint ? (
+          <p className={styles.hint} onClick={sss}>
+            {props.hint}
+          </p>
+        ) : (
+          <button className={styles.show_hint} onClick={sss}>
+            힌트
+          </button>
+        )}
+        {!showAns && (
+          <button className={styles.answer} onClick={ddd}>
+            정답 확인
+          </button>
+        )}
+        <div className={styles.clear_both} />
       </div>
-      {showHint ? (
-        <p className={styles.hint} onClick={sss}>
-          {props.hint}
-        </p>
-      ) : (
-        <button className={styles.show_hint} onClick={sss}>
-          힌트
-        </button>
-      )}
-      {!showAns && (
-        <button className={styles.answer} onClick={ddd}>
-          정답 확인
-        </button>
-      )}
-
-      <div className={styles.clear_both} />
     </div>
   );
 }
